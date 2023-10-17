199f72bdc772cf0703aeb7867996863c9a24cb07
Swam Yay Pyae Hmuu <swamyaypyaehmuu@gmail.com>
2023-10-17 11:43:01 +0800

    another

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ const PORT =parseInt(process.env.PORT) || 3000
 
 //create an instance of express
 const app = express()
+
+//Serve file in the static dir
+app.use(express.static(__dirname+'/static'))
+
 app.listen(PORT,()=>{
     console.info(`Application started on port ${PORT} at ${new Date()}`)
 })
